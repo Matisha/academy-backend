7c5b34c0f5d994af5017ef40564e21a20b0c98cc
Mathias Schoen <schomath@gmail.com>
2026-03-01 01:56:04 -0800

    okay

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,18 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+//   server: {
+//     port: 3000,
+//     host: '0.0.0.0'
+//   }
+// });
+
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // base: '/fm-academy/',
       server: {
         port: 3000,
         host: '0.0.0.0',
